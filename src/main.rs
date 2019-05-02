e26e53e080b861eb31921221a6e3edb628bef886
Vadim Kaushan <admin@disasm.info>
2019-05-02 13:29:15 +0200

    Add code for perf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use matcher::{create_orders, OrderBook, Order, OrderKind, OrderSide};
+
 fn main() {
-    println!("Hello, world!");
+    let orders = create_orders();
+    let mut book = OrderBook::new();
+    for order in orders {
+        book.execute_order(order);
+    }
+
+    let order = Order {
+        price_limit: 10020,
+        amount: 1000,
+        user_id: 0,
+        kind: OrderKind::Limit,
+        side: OrderSide::Buy
+    };
+
+    for _ in 0..100000 {
+        let mut book = book.clone();
+        let order = order.clone();
+        book.execute_order(order);
+    }
 }
