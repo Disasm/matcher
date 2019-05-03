1ca7782fe13004137378ad704fb1506cbd133d74
Vadim Kaushan <admin@disasm.info>
2019-05-03 11:40:29 +0200

    Remove commented code

--- src/queues/reversed_vec.rs
+++ src/queues/reversed_vec.rs
@@ -6,53 +6,6 @@ use std::{slice, iter};
 #[derive(Clone)]
 pub struct ReversedVec<D>(Vec<Order<D>>);
 
-/*impl<D> ReversedVec<D> {
-    pub fn new() -> Self {
-        Self(Vec::new())
-    }
-
-    #[inline]
-    pub fn len(&self) -> usize {
-        self.0.len()
-    }
-
-    #[inline]
-    pub fn push_front(&mut self, item: T) {
-        self.0.push(item);
-    }
-
-    #[inline]
-    pub fn push_back(&mut self, item: T) {
-        self.0.insert(0, item);
-    }
-
-    #[inline]
-    pub fn insert(&mut self, index: usize, item: T) {
-        assert!(index < self.0.len());
-
-        if index == 0 {
-            self.0.push(item)
-        } else {
-            self.0.insert(self.0.len() - index, item);
-        }
-    }
-
-    #[inline]
-    pub fn truncate_front(&mut self, len: usize) {
-        self.0.truncate(len)
-    }
-
-    #[inline]
-    pub fn iter(&self) -> impl DoubleEndedIterator<Item=&T>+ExactSizeIterator<Item=&T> {
-        self.0.iter().rev()
-    }
-
-    #[inline]
-    pub fn iter_mut(&mut self) -> impl DoubleEndedIterator<Item=&mut T>+ExactSizeIterator<Item=&mut T> {
-        self.0.iter_mut().rev()
-    }
-}*/
-
 impl<D> Default for ReversedVec<D> {
     fn default() -> Self {
         Self(Vec::new())
