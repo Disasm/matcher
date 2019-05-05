72a93d14f829bf0121ab233885780502db79fe2f
Vadim Kaushan <admin@disasm.info>
2019-05-05 15:30:22 +0200

    Implement reading from file and writing log to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,29 @@
-#![allow(unused)]
-use matcher::{create_orders, OrderBook, dump20};
-use matcher::order::{IncomingOrder, OrderKind, OrderSide};
-use matcher::GoodEnoughQueue;
-use matcher::log::{ExecutionLogger, DummyLogger};
+use std::env;
+use matcher::OrderBook;
+use matcher::log::VectorLogger;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 fn main() {
-    let orders = create_orders();
-    let mut book = OrderBook::deserialize(orders);
-    let mut logger = DummyLogger;
-    assert_eq!(book.bid().len(), 3500);
-    assert_eq!(book.ask().len(), 3500);
+    let mut args = env::args_os();
+    if args.len() < 2 {
+        eprintln!("Usage: matcher <filename>");
+        return;
+    }
+    let filename = args.nth(1).unwrap();
 
-    let order = IncomingOrder {
-        price_limit: 10020,
-        size: 200,
-        user_id: 0,
-        kind: OrderKind::Limit,
-        side: OrderSide::Buy
-    };
+    let f = File::open(filename).expect("invalid filename");
+    let f = BufReader::new(f);
 
-    //dump20(&book);
-    for _ in 0..1000000 {
-        book.execute_order(order.clone(), &mut logger);
-
-        //dump20(&book);
-
-        assert_eq!(book.bid().len(), 3500);
-        assert_eq!(book.ask().len(), 3500 - 20);
-
-        for i in (0..20).rev() {
-            let order = IncomingOrder {
-                price_limit: 10000 + i + 1,
-                size: 10,
-                user_id: 100 + i,
-                kind: OrderKind::Limit,
-                side: OrderSide::Sell
-            };
-            book.execute_order(order, &mut logger);
+    let mut book = OrderBook::new();
+    for line in f.lines() {
+        let line = line.unwrap();
+        //println!("{}", line);
+        let order = line.parse().expect("can't parse order");
+        let mut logger = VectorLogger::new();
+        book.execute_order(order, &mut logger);
+        for log_item in logger.as_slice() {
+            println!("{}", log_item.to_string());
         }
-        //dump20(&book);
-        assert_eq!(book.bid().len(), 3500);
-        assert_eq!(book.ask().len(), 3500);
     }
 }
